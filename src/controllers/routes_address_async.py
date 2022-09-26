fae744fe584a6971a4b292d2507ae145d69f244a
anavitoria <anavitoriasln@gmail.com>
2022-09-25 22:42:49 -0300

    refactoring all routes

--- src/controllers/routes_address_async.py
+++ src/controllers/routes_address_async.py
@@ -1,18 +1,19 @@
-import collections
-from bson.objectid import ObjectId
-from fastapi import APIRouter, Request, Response, Body, HTTPException, status
-from src.schemas.address import AddressSchema, Address
-from fastapi.encoders import jsonable_encoder
 from typing import List
+
+from bson.objectid import ObjectId
+from fastapi import APIRouter, Request, Body, status
+from fastapi.encoders import jsonable_encoder
+
 from src.models.address import create_address
-from src.models.address import *
+from src.schemas.address import Address
+
 
 router = APIRouter()
 
 
-
-@router.put("/{user_id}", response_description="create a new address", status_code=status.HTTP_201_CREATED, response_model= List[Address])
-async def route_address(user_id: str, request:Request, new_address: List[Address] = Body(...)):
+@router.put("/{user_id}", response_description="create a new address", status_code=status.HTTP_201_CREATED,
+            response_model=List[Address])
+async def route_address(user_id: str, request: Request, new_address: List[Address] = Body(...)):
     new_address = list(map(lambda end: jsonable_encoder(end), new_address))
     data_address = await create_address(request.app.database.address_collection,
                                         request.app.database.users_collection,
