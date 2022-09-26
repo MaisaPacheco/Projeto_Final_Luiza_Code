fd957ea8fa59f9f57044f1b15e6aef944001abc3
anavitoria <anavitoriasln@gmail.com>
2022-09-25 21:35:03 -0300

    add pydantic validation for objectId

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,10 +1,11 @@
 from typing import Optional
 from pydantic import BaseModel, Field
 from pydantic.networks import EmailStr
+from src.utils.pydantic_objectId import PyObjectId
 
 
 class UserSchema(BaseModel):
-
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     email: EmailStr = Field(unique=True, index=True)
     password: str
     is_active: bool = Field(default=True)
